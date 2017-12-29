2c748df3f288db9b657319fa7613eb1a542b21c8
erostark <eioncontaste@ac.c4q.nyc>
2017-12-29 17:15:02 -0500

    working assign to specific pad

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var loadedFileAudioBuffer = null; //this is where we store the decoded audio data from our file
+var loadedFileAudioBuffers = []; //this is where we store the decoded audio data from our file
 
 var browserAudioLibrary = new AudioContext();//window.webkitAudioContext(); //this is what the browser gives us to process audio stuff
 
@@ -12,16 +12,31 @@ const setup = () => {
     buttons.forEach(function(element) {
         element.addEventListener('click', playSound);
     }, this);
-    
 
+    var assign = document.querySelectorAll('.padAssign');
+    assign.forEach(function(element){
+        element.addEventListener('click', assignPad);
+    })
+};
+
+
+const assignPad = (event) => {
+
+    var assign = document.querySelectorAll('.padAssign');
+    assign.forEach(function(element){
+        element.classList.remove('assignHighlighted');
+    })
+
+    var clickedButton = event.currentTarget;
+    clickedButton.classList.add('assignHighlighted');
 };
 
 const filePicked = (event) => {
     console.log("file pic")
     var reader = new FileReader();
     reader.onload = function(e){
-        initSound(this.result);
         console.log("file finished loading")
+        initSound(this.result);
     }
    
     reader.readAsArrayBuffer(event.currentTarget.files[0])
@@ -30,17 +45,28 @@ const filePicked = (event) => {
 const initSound = (soundBuffer) => {
     console.log("now we need to process uadio from loaded file")
     browserAudioLibrary.decodeAudioData(soundBuffer, function(buffer) {
-        loadedFileAudioBuffer = buffer;
+
+        var assign = document.querySelector('.assignHighlighted');
+        var whichButton = assign.//substring of button id #
+        
+        loadedFileAudioBuffers[whichButton] = buffer;
         console.log("stored file", loadedFileAudioBuffer);
-        playSound();
+        assign.classList.remove('assignHighlighted');
+//        playSound();
     }, function(e) {
         console.log('Error decoding file', e);
     });     
 };
 
 const playSound = (event) => {
+
+    var clickedButton = event.currentTarget;
+    clickedButton.classList.add('padHighlighted');
+
+    var whichButton = clickedButton.//substring of button id #
+
     player = browserAudioLibrary.createBufferSource();
-    player.buffer = loadedFileAudioBuffer;
+    player.buffer = loadedFileAudioBuffers[whichButton];
     player.loop = false;
     player.connect(browserAudioLibrary.destination);
     player.start(); // Play immediately.
