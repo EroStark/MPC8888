00dba43eb9b975db5039e9a348d52d7d33e55e3b
erostark <eioncontaste@ac.c4q.nyc>
2017-12-29 16:23:12 -0500

    Added play sound onCLick

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+var loadedFileAudioBuffer = null; //this is where we store the decoded audio data from our file
+
+var browserAudioLibrary = new AudioContext();//window.webkitAudioContext(); //this is what the browser gives us to process audio stuff
+
+
 const setup = () => {
     var fileInput = document.querySelector('input[type="file"]');
     fileInput.addEventListener('change',filePicked, false);
 
+    var buttons = document.querySelectorAll('.pad');
+    console.log(buttons);
+    buttons.forEach(function(element) {
+        element.addEventListener('click', playSound);
+    }, this);
+    
 
 };
 
@@ -18,6 +29,22 @@ const filePicked = (event) => {
 
 const initSound = (soundBuffer) => {
     console.log("now we need to process uadio from loaded file")
+    browserAudioLibrary.decodeAudioData(soundBuffer, function(buffer) {
+        loadedFileAudioBuffer = buffer;
+        console.log("stored file", loadedFileAudioBuffer);
+        playSound();
+    }, function(e) {
+        console.log('Error decoding file', e);
+    });     
+};
+
+const playSound = (event) => {
+    player = browserAudioLibrary.createBufferSource();
+    player.buffer = loadedFileAudioBuffer;
+    player.loop = false;
+    player.connect(browserAudioLibrary.destination);
+    player.start(); // Play immediately.
+    
 };
 
 console.log("hello mpc");
